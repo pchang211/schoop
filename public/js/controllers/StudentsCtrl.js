f532674c90462bbaa0e8c5160a8a02bc38ad4616
Philip Chang <pchang211@gmail.com>
2014-09-04 21:28:38 -0700

    Added some css and table to display GET response data

--- public/js/controllers/StudentsCtrl.js
+++ public/js/controllers/StudentsCtrl.js
@@ -1,11 +1,19 @@
 var StudentsCtrl = angular.module('StudentsCtrl', ['StudentsService']).controller('studentsController', function($scope, studentsRepository) {
 	$scope.message = "This is the students page";
 
+	$scope.helloworld = function() {
+		console.log('hello world');
+	}
+
 	$scope.getStudents = function() {
 
-		studentsRepository.get().success(function(students) {
-			console.log(students);
+		studentsRepository.get().success(function(data) {
+			$scope.students = data;
+			console.log($scope.students);
 		});
 
 	};
+
+	$scope.students = $scope.getStudents();
+
 });
