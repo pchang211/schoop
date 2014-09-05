a58b09b38caf5eb162634b912a3e8aad7e513aa4
Philip Chang <pchang211@gmail.com>
2014-09-04 20:30:13 -0700

    Added students service to be able to interact with api from students controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,1 @@
-// angular.module('ccgcApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'StudentsCtrl']);
-
 angular.module('ccgcApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'StudentsCtrl', 'StudentsService']);
